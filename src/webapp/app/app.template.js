4cb917cef5064ce8e75328083385c807ea815869
endeea <nbanzyme@gmail.com>
2019-04-10 23:51:22 +0200

    add styles for app intro

--- src/webapp/app/app.template.js
+++ src/webapp/app/app.template.js
@@ -5,5 +5,11 @@ export const AppTemplate = (props) => {
             <h1>${props.appName}</h1>
         </header>
     </section>
+
+    <section class="section intro">
+        <h3>What is Rota?</h3>
+        <p> Rota helps you to generate equitable
+        work schedules quickly!</p>
+    </section>
     `
 }
