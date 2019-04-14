35a9e8d987b05ef20f58cdad6d9fb5d868c46e1e
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-04-14 12:17:27 +0200

    add more input on plus click

--- src/webapp/app/pages/components/create-roster-form/create-roster.component.js
+++ src/webapp/app/pages/components/create-roster-form/create-roster.component.js
@@ -1,48 +1,73 @@
-import './create-roster.css';
-import { CreateRosterTemplate } from './create-roster.template';
+import "./create-roster.css";
+import { CreateRosterTemplate } from "./create-roster.template";
 
-export class CreateRosterComponent{
-    
-    constructor(model){
-        this.state = model;
-    }
-
-    render(){
-        return CreateRosterTemplate(this.state);
-    }
-}
-
-
-function getInputCollections(elementId){
-    const sections =  document.getElementsByClassName(elementId);
-    return Array.from(sections);
-}
-
-function getFormValue(){
-    const span = document.getElementById('span').value;
-    const sectionNames = getInputCollections('section-input');
-    const staffNmes = getInputCollections('section-input');
-
-    return {
-        spanIndays: span,
-        sections: sectionNames.map(item => item.value),
-        staff: staffNmes.map(item => item.value),
-    }
-}
-
-
-function addEventListener$(){
-    document.getElementById('generate-rooster').addEventListener('click', ()=>{
-        console.log(getFormValue());
-    });
-}
-
-if (document.readyState !== "loading") {
- 
-    addEventListener$();
-  
-  } else {
-    document.addEventListener("DOMContentLoaded", function() {
-        addEventListener$();
-    });
+export class CreateRosterComponent {
+  constructor(model) {
+    this.state = model;
   }
+
+  render() {
+    return CreateRosterTemplate(this.state);
+  }
+}
+
+// After render
+if (document.readyState !== "loading") {
+  addEventListener$();
+} else {
+  document.addEventListener("DOMContentLoaded", function() {
+    addEventListener$();
+  });
+}
+
+function addEventListener$() {
+
+  document.getElementById("generate-rooster").addEventListener("click", () => {
+    console.log(getFormValue());
+  });
+
+  document.getElementById("staff-input-add").addEventListener("click", ()=>{
+      addInputToForm("roster-form-container", "staff-input");
+  });
+
+  document.getElementById("section-input-add").addEventListener("click", ()=>{
+    addInputToForm("roster-form-container", "section-input");
+  });
+}
+
+function getInputCollections(elementId) {
+  const sections = document.getElementsByClassName(elementId);
+  return Array.from(sections);
+}
+
+function getFormValue() {
+  const span = document.getElementById("span").value;
+  const sectionNames = getInputCollections("section-input");
+  const staffNmes = getInputCollections("staff-input");
+
+  return {
+    spanIndays: span,
+    sections: sectionNames.map(item => item.value),
+    staff: staffNmes.map(item => item.value)
+  };
+}
+
+function getPlaceholderMsg(areaId){
+    return areaId == "section-input"? "Name of work section" : "Name of staff memeber";
+}
+
+function addInputToForm(formId, areaId){
+    const existingInputsCount = getInputCollections(areaId).length;
+    const formContainer = document.getElementById(formId);
+    const inputElem = document.createElement("INPUT");
+    inputElem.setAttribute("placeholder", getPlaceholderMsg(areaId));
+    inputElem.setAttribute("class", areaId);
+
+    if(areaId == 'section-input'){
+        inputElem.setAttribute('id', `section${existingInputsCount + 1}`);
+    }else{
+        inputElem.setAttribute('id', `staff${existingInputsCount + 1}`);
+    }
+
+    formContainer.appendChild(inputElem);
+}
--- src/webapp/app/pages/components/create-roster-form/create-roster.template.js
+++ src/webapp/app/pages/components/create-roster-form/create-roster.template.js
@@ -6,18 +6,19 @@ export const CreateRosterTemplate = (model) => {
         Simply fill in the required fields 
         below to generate your new roster.
     </small>
-        <form class="centered-form">
+    <form class="centered-form" id="roster-form-container">
         <input class="section-input" type="text" placeholder="Rooste span in days" id="span"/>
-        
         <hr>
+
         <input class="staff-input" placeholder="Name of staff memeber" id="staff1"/>
         <input class="staff-input" placeholder="Name of staff member" id="staff2"/>
         <input class="staff-input" placeholder="Name of staff member" id="staff3"/>
-        <i class="fas fa-plus-circle"></i>
+        <i id="staff-input-add" class="fas fa-plus-circle"></i>
         <hr>
+
         <input class="section-input" placeholder="Name of work section e.g. Packaging" id="section1"/>
         <input class="section-input" placeholder="Name of work section e.g. Operations" id="section2"/>
-        <i class="fas fa-plus-circle"></i>
+        <i  id="section-input-add" class="fas fa-plus-circle"></i>
         <button type="button" id="generate-rooster">Generate roster</button>
     </form>
     </main>
