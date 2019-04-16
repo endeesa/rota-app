f08d1387315e581c342fb6960094c5ed070a6c54
N Nemakhavhani <ndamulelo@bbd.co.za>
2019-04-16 15:05:14 +0200

    Clean up aws lambda service caller

--- src/webapp/app/shared/service-callers/aws_lambda.service.js
+++ src/webapp/app/shared/service-callers/aws_lambda.service.js
@@ -1,58 +1,31 @@
 // Handles calls to API to fetch and update rooster
+export function MakeAsyncGetRequest(endpoint, params, key) {
+  const http = new XMLHttpRequest();
+  const queryStringParams = queryStringBuilder(params);
+  const url = `${endpoint}?${queryStringParams}`;
+  http.open("GET", url);
+  // http.setRequestHeader('x-api-key', key);
+  // http.setRequestHeader('Access-Control-Allow-Origin', '*');
+  http.send();
 
-// export const GenerateNewRoster = () =>{
-//     return null;
-// }
+  return new Promise((resolve, reject) => {
+    http.onreadystatechange = function() {
+      if (this.status == 200) {
+        const response = JSON.parse(http.responseText);
+        resolve(response);
+      } else {
+        reject(`Sever error - Please try again.`);
+      }
+    };
+  });
 
+  function queryStringBuilder(params) {
+    let result = "";
 
-// export const GetDailyRoster = (groupId, UID)=> {
-//     return null;
-// }
-
-// export const GetMothlyRoster = (groupId, UID) => {
-//     return null;
-// }
-// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-function queryStringBuilder(params){
-    let result = '';
-
-    for(let key in params){
-        result += `${key}=${params[key]}&`;
+    for (let key in params) {
+      result += `${key}=${params[key]}&`;
     }
 
-    return result.slice(0,-1);
-}
-
-
-// HTTP Helpers
-export function MakeAsyncGetRequest(endpoint, params, key){
-    const http = new XMLHttpRequest();
-    const queryStringParams = queryStringBuilder(params);
-    const url = `${endpoint}?${queryStringParams}`;
-    console.log('AWS lambda service caller - Check url: ', url);
-    http.open('GET', url);
-    // http.setRequestHeader('x-api-key', key);
-    // http.setRequestHeader('Access-Control-Allow-Origin', '*');
-    // Object.keys(localDB).length != parseInt(params.span)
-    http.send();
-
-    return new Promise( (resolve, reject)=> {
-
-        http.onreadystatechange = function() {
-
-            if (this.status == 200) {
-                const response = JSON.parse(http.responseText);
-                console.log('Remote Api response: ', response);
-               
-                resolve(response);
-            }else{
-                reject(`Sever error - Please try again.`);
-            }
-        }
-
-    });
-
-
-
+    return result.slice(0, -1);
+  }
 }
