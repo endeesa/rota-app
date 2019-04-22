7b5742d0311efb88ccd7e82726f8c9ca5a502849
endeea <nbanzyme@gmail.com>
2019-04-22 13:46:59 +0200

    update app styles

--- src/webapp/app/app.template.js
+++ src/webapp/app/app.template.js
@@ -9,36 +9,10 @@ export const AppTemplate = (props) => {
     </section>
 
     <section class="section intro mb-2">
-        <h3>What is Rota?</h3>
-        <p> Rota helps you to generate equitable
-        work schedules quickly!</p>
-    </section>
+    <h3>What is Rota?</h3>
+    <p> Rota helps you to generate equitable
+    work schedules quickly!</p>
+</section>
 
-    <aside id="rooter-placeholder"><aside/>
-    <section id="rooster-container" class="content">
-    Jan 2016 ,32 - A B C
-    </section>
-
-    <section class="section participants">
-        <h2 class="text-primary"> Generate roster </h2>
-        <small> Add the names of the participants below to get started.</small> 
-        <form class="my-4">
-        <div class="form-field-wrapper">
-            <input class="form-field" placeholder="Name of participant 1" id="name-1"/>
-            <span class="shadow-form-field"></span>
-        </div>
-
-        <div class="form-field-wrapper">
-            <input class="form-field" placeholder="Name of participant 1" id="name-1"/>
-            <span class="shadow-form-field"></span>
-        </div>
-
-        <div class="form-field-wrapper">
-            <input class="form-field" placeholder="Name of participant 2"  id="name-2"/><i id="add-more-btn" class="fas fa-user-plus ml-1"></i>
-            <span class="shadow-form-field"></span>
-        </div>
-        </form>
-        <button id="generate-roster-btn">Generate roster<i class="fas fa-step-forward ml-1"></i></button>
-    </section>
     `
 }
