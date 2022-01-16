83378895d62562bd01dc2e98f82d99aa0005e58f
endeesa <endeesa@bbd.extra>
2022-01-17 01:18:00 +0200

    use frontend params to genrate a roster

--- api/RosterEngine/__init__.py
+++ api/RosterEngine/__init__.py
@@ -39,10 +39,12 @@ def prepare_schedule(staff=5, sections=3, span=30) -> Dict:
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # staff = req.params.get('staff')
-    # TODO: take params from frontend
-    generated_roster = prepare_schedule()
-    logging.info(f"Successfully generated {len(generated_roster)} slots")
+    span = req.params.get('span')
+    staff = req.params.get('staff')
+    sections = req.params.get('sections')
+
+    generated_roster = prepare_schedule(staff=int(staff), span=int(span), sections=int(sections))
+    logging.debug(f"Successfully generated {len(generated_roster)} slots")
 
     return func.HttpResponse(
         json.dumps(generated_roster),
