58ef55554906b4289492958325e2be7015c05949
endeesa <endeesa@bbd.extra>
2022-01-16 23:56:33 +0200

    build app locally

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 . ~/.bashrc
+
+here=$(pwd)
+cd site
+npm run build-prod
+cd $here
+
+if [ "$?" -ne 0 ]
+then
+    echo "Build failed, abort deployment!!"
+    exit 1
+fi
+
 parameterFile="@./azure/az_staticwebapp.parameters.json"
 az group create -n $RG -l $LOCATION --verbose -o yamlc
 az deployment group create --name rot-app-release$RANDOM \
